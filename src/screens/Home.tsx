d09a31b941b9bb076ea92ce413621bfdbe57990f
Kevin <kmcdermott31@gmail.com>
2021-11-02 22:45:31 +0000

    Added some stuff to test on mac

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { Button, SafeAreaView, StyleSheet, Text, View, Image } from 'react-native'
 
 const Home = () => {
     return (
         <SafeAreaView style={styles.mainContainer}>
             <Text style={styles.text}>Rapid Rock Paper Scissors</Text>
+            <Image style={{width: '20%'}} source={require('../../assets/icons/9-2-rock-png.png')}/>
             <Button title={'Play'} onPress={() => alert('Howdy!!!')}/>
+            <Button title={'Settings'} onPress={() => alert('Howdy!!!')}/>
+            <Button title={'Contact'} onPress={() => alert('Howdy!!!')}/>
         </SafeAreaView>
     )
 }
